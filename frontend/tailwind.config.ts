24621d1c8c74d812a911280f01e546ec2edd0325
William He <contact@williamhe.dev>
2025-02-11 00:10:11 -0800

    feat: extend colours

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,16 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        'background-primary': '#ECE8EF',
+        primary: '#4392F1',
+        secondary: '#E3EBFF',
+        accent: '#DC493A',
+        'text-primary': '#1E1E1E',
+        'grade-good': '#37A52E',
+        'grade-fair': '#E98223',
+        'grade-poor': '#F16363',
+        'course-completed': '#88EF64',
+        'course-in-progress': '#EFBC59',
       },
     },
   },
