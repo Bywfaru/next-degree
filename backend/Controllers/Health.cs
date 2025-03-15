92afd35be8bce4dcddff908f917ff619e21c3492
William He <contact@williamhe.dev>
2025-03-15 13:07:23 -0700

    feat: add health/ping endpoint

---
+++ backend/Controllers/Health.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DegreePlanner.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class Health : ControllerBase
+{
+    [HttpGet("ping")]
+    public ActionResult<string> GetPing()
+    {
+        return Ok("Pong");
+    }
+}
